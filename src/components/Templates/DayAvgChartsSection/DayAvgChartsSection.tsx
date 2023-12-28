0c27c5c02fec1fb2358e034a6403a2323628e7c3
Mantvydas Liobė <mantvydasliobe@gmail.com>
2023-12-28 11:23:55 +0200

    refactore(charts): DRY code

--- src/components/Templates/DayAvgChartsSection/DayAvgChartsSection.tsx
+++ src/components/Templates/DayAvgChartsSection/DayAvgChartsSection.tsx
@@ -2,14 +2,15 @@
 
 import { useEffect } from 'react';
 
-import { Typography, CircularProgress, Box } from '@mui/material';
+import { Typography, Box } from '@mui/material';
 import Grid2 from '@mui/material/Unstable_Grid2/Grid2';
 
 import ChartAvgDay from '@/components/Atoms/ChartAvgDay/ChartAvgDay';
+import ErrorsChartsData from '@/components/Molecules/ErrorsChartsData/ErrorsChartsData';
 import { useGraphData } from '@/store/graphData';
 import { getAvgDayData } from '@/utils/getAvgDayData';
 
-import classes from '../../../app/sensor-data/[id]/page.module.css';
+import classes from '@/app/sensor-data/[id]/page.module.css';
 
 const DayHourlyChartsSection = ({ params }: { params: string }) => {
     const { startRangeDay, endRangeDay, avgGraphData, setAvgGraphData } =
@@ -30,7 +31,7 @@ const DayHourlyChartsSection = ({ params }: { params: string }) => {
     return (
         <>
             {avgGraphData !== null && (
-                <Box className={classes.inputContainer}>
+                <Box className={classes.dataDateContainer}>
                     <Typography variant="h4" align="center">
                         Duomenų data nuo {startRangeDay} iki {endRangeDay}
                     </Typography>
@@ -38,45 +39,23 @@ const DayHourlyChartsSection = ({ params }: { params: string }) => {
             )}
 
             <Grid2 container className={classes.graphsContainer}>
-                {/* Case when loading data */}
-                {avgGraphData === null && (
-                    <Grid2 xs={12} alignSelf="center">
-                        <Typography textAlign="center">
-                            <CircularProgress size={55} />
-                        </Typography>
-                    </Grid2>
-                )}
+                <ErrorsChartsData graphData={avgGraphData} />
 
-                {/* Case when there is an error from server */}
-                {avgGraphData !== null && 'error' in avgGraphData && (
-                    <Grid2 xs={12} alignSelf="center">
-                        <Typography textAlign="center" variant="h5" color="red">
-                            🛑 {avgGraphData.error} 🛑
-                        </Typography>
-                    </Grid2>
-                )}
-
-                {/* Case when there is data from server */}
                 {avgGraphData !== null &&
                     'data' in avgGraphData &&
-                    //    Case when data object is empty from server
-                    (avgGraphData.data.length == 0 ? (
-                        <Grid2 xs={12} alignSelf="center">
-                            <Typography variant="h5" textAlign="center">
-                                Pasirinktai dienai nėra duomenų.
-                            </Typography>
-                        </Grid2>
-                    ) : (
-                        // Case when there is data and data in array from server
-                        avgGraphData.data.map(
-                            // NEED TO CHANGE ANY TYPE
-                            (singleParameterData: any, key: number) => (
-                                <Grid2 key={key} xs={12} md={6} xl={4}>
-                                    <ChartAvgDay array={singleParameterData} />
-                                </Grid2>
-                            )
-                        )
-                    ))}
+                    avgGraphData.data.length > 0 && (
+                        <>
+                            {avgGraphData.data.map(
+                                (singleParameterData: any, key: number) => (
+                                    <Grid2 key={key} xs={12} md={6} xl={4}>
+                                        <ChartAvgDay
+                                            array={singleParameterData}
+                                        />
+                                    </Grid2>
+                                )
+                            )}
+                        </>
+                    )}
             </Grid2>
         </>
     );
--- src/components/Templates/DayHourlyChartsSection/DayHourlyChartsSection.tsx
+++ src/components/Templates/DayHourlyChartsSection/DayHourlyChartsSection.tsx
@@ -2,14 +2,15 @@
 
 import { useEffect } from 'react';
 
-import { Typography, CircularProgress, Box } from '@mui/material';
+import { Typography, Box } from '@mui/material';
 import Grid2 from '@mui/material/Unstable_Grid2/Grid2';
 
 import DayHourlyChart from '@/components/Atoms/DayHourlyChart/DayHourlyChart';
+import ErrorsChartsData from '@/components/Molecules/ErrorsChartsData/ErrorsChartsData';
 import { getDayData } from '@/utils/getDayData';
 import { useGraphData } from '@/store/graphData';
 
-import classes from '../../../app/sensor-data/[id]/page.module.css';
+import classes from '@/app/sensor-data/[id]/page.module.css';
 
 const DayHourlyChartsSection = ({ params }: { params: string }) => {
     const { date, graphData, setGraphData } = useGraphData();
@@ -19,67 +20,37 @@ const DayHourlyChartsSection = ({ params }: { params: string }) => {
             const data = await getDayData(params, date);
             setGraphData(data);
         };
-        // date ? fetchDayHourlyData() : fetchAvgDayData();
-
         fetchDayHourlyData();
     }, [date]);
 
     return (
         <>
             {graphData !== null && (
-                <Box className={classes.inputContainer}>
+                <Box className={classes.dataDateContainer}>
                     <Typography variant="h4" align="center">
                         Duomenų data {date}
                     </Typography>
                 </Box>
             )}
 
-            <Grid2
-                container
-                className={classes.graphsContainer}
-                // sx={{ width: `calc(95vw - ${drawerWidth}` }}
-            >
-                {/* Case when loading data */}
-                {graphData === null && (
-                    <Grid2 xs={12} alignSelf="center">
-                        <Typography textAlign="center">
-                            <CircularProgress size={55} />
-                        </Typography>
-                    </Grid2>
-                )}
+            <Grid2 container className={classes.graphsContainer}>
+                <ErrorsChartsData graphData={graphData} />
 
-                {/* Case when there is an error from server */}
-                {graphData !== null && 'error' in graphData && (
-                    <Grid2 xs={12} alignSelf="center">
-                        <Typography textAlign="center" variant="h5" color="red">
-                            🛑 {graphData.error} 🛑
-                        </Typography>
-                    </Grid2>
-                )}
-
-                {/* Case when there is data from server */}
                 {graphData !== null &&
                     'data' in graphData &&
-                    //    Case when data object is empty from server
-                    (graphData.data.length == 0 ? (
-                        <Grid2 xs={12} alignSelf="center">
-                            <Typography variant="h5" textAlign="center">
-                                Pasirinktai dienai nėra duomenų.
-                            </Typography>
-                        </Grid2>
-                    ) : (
-                        // Case when there is data and data in array from server
-                        graphData.data.map(
-                            // NEED TO CHANGE ANY TYPE
-                            (singleParameterData: any, key: number) => (
-                                <Grid2 key={key} xs={12} md={6} xl={4}>
-                                    <DayHourlyChart
-                                        array={singleParameterData}
-                                    />
-                                </Grid2>
-                            )
-                        )
-                    ))}
+                    graphData.data.length > 0 && (
+                        <>
+                            {graphData.data.map(
+                                (singleParameterData: any, key: number) => (
+                                    <Grid2 key={key} xs={12} md={6} xl={4}>
+                                        <DayHourlyChart
+                                            array={singleParameterData}
+                                        />
+                                    </Grid2>
+                                )
+                            )}
+                        </>
+                    )}
             </Grid2>
         </>
     );
